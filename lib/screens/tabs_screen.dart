28411e9c0c830d452d32795c35051186ead12f43
Shreyash Pandey <shreyash.edu@gmail.com>
2022-04-25 23:29:38 +0530

    Added BottomNavigationBar and A drawer

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/screens/categories_screen.dart';
 import 'package:meals_app/screens/favourite_screen.dart';
+import 'package:meals_app/widgets/main_drawer.dart';
 
 class TabsScreen extends StatefulWidget {
   @override
@@ -8,24 +9,48 @@ class TabsScreen extends StatefulWidget {
 }
 
 class _TabsScreenState extends State<TabsScreen> {
+  List<Widget> arr = [
+    CategoriesScreen(),
+    FavouriteScreen(),
+  ];
+  int selectedPageIndex = 0;
+  void _selectededPage(int index) {
+    setState(() {
+      selectedPageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
       length: 2,
       child: Scaffold(
         appBar: AppBar(
-            title: const Text('Meals'),
-            bottom: const TabBar(tabs: [
-              Tab(
-                icon: Icon(Icons.category),
-                text: 'Categories',
-              ),
-              Tab(
-                icon: Icon(Icons.favorite),
-                text: 'Favourites',
-              )
-            ])),
-        body: TabBarView(children: [CategoriesScreen(), FavouriteScreen()]),
+          title:
+              Text(selectedPageIndex == 0 ? 'Categories' : 'Your Favourites'),
+        ),
+        drawer: Drawer(child: MainDrawer()),
+        body: arr[selectedPageIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          onTap: _selectededPage,
+          backgroundColor: Theme.of(context).primaryColor,
+          selectedItemColor: Colors.black,
+          unselectedItemColor: Colors.white,
+          type: BottomNavigationBarType.shifting,
+          currentIndex: selectedPageIndex,
+          items: [
+            BottomNavigationBarItem(
+              backgroundColor: Theme.of(context).primaryColor,
+              icon: const Icon(Icons.category),
+              label: 'Categories',
+            ),
+            BottomNavigationBarItem(
+              backgroundColor: Theme.of(context).primaryColor,
+              icon: const Icon(Icons.favorite_outlined),
+              label: 'Favourites',
+            ),
+          ],
+        ),
       ),
     );
   }
